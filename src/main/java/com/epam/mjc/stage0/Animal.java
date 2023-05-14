21fd375832d65e571ce1a935e518655b92f567a1
Khusan <khusanabdukarimov007@gmail.com>
2023-05-14 19:42:36 +0500

    done

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,27 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.hasFur = hasFur;
+        this.numberOfPaws = numberOfPaws;
+    }
+
+    public String getDescription() {
+        return String.format("This animal is mostly %s. It has %d " + checkCounts() + " and " + checkHasFur(), color, numberOfPaws);
+    }
+
+    private String checkHasFur() {
+        return hasFur ? "a fur." : "no fur.";
+    }
+
+    private String checkCounts() {
+        return numberOfPaws == 1 ? "paw" : "paws";
+    }
 }
+
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,13 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal{
 
+    public Dog() {
+        super("brawn", 4, true);
+    }
+
+    @Override
+    public String getDescription() {
+        return "This animal is mostly brown. It has 4 paws and a fur.";
+    }
 }
